df1b146606227c4e0956d93d656b77c5d19d0ee0
sagar poudel <poudelsagar@icloud.com>
2025-08-19 18:27:51 +0900

    agian in short time

--- 9-hw/src/App.jsx
+++ 9-hw/src/App.jsx
@@ -1,52 +1,53 @@
 import React, { useState } from 'react'
 
 const  App = () => {
-  const [newTasks, setNewTask] = useState("");
-  const handelChange = (e)=>{
+   const [newTasks, setNewTask] = useState("");
+   const handelChange  = (e) =>{
     setNewTask(e.target.value);
-  } ;
-  const [tasks, setTasks ] = useState([]);
-  const handelAdd = () =>{
-    const task = {
-      id: Math.random()+ Date.now(),
+   };
+ 
+   const [tasks, setTasks] = useState([]);
+   const handelAdd = ()=>{
+    const upDatedTasks = {
+      id: Math.random() + Date.now(),
       text: newTasks,
       completed : false,
     };
-    setTasks([...tasks, task]);
-    setNewTask("");
-  }
-  const handelDone = (id)=>{
-   const upDatedTasks =  tasks.map((task)=>
-      id === task.id ? { ...task , completed: !task.completed} : task
-    );
+    setTasks([...tasks, upDatedTasks]);
+    setNewTask("")
+   };
+   const handelDone = (id)=>{
+    const upDatedTasks = tasks.map((task)=>(
+      id === task.id ? {...task, completed : !task.completed} : task
+    ));
     setTasks(upDatedTasks);
+    
+   };
 
+  const handelDelete = (id) =>{
+    const confirmDeletion = confirm ("are you sure want to delete");
+    if (confirmDeletion) {
+const updatedTasks = tasks.filter ((task)=>
+      id !== task.id 
+    );
+    setTasks (updatedTasks);
+    }
+    
   }
-  const handelDelete = (id)=>{
-    const upDatedTasks = tasks.filter ((task)=> 
-    id !== task.id );
-    setTasks(upDatedTasks);
-  };
   return (
     <div>
-      <h1>To  Do App</h1>
+      <h1>To Do App</h1>
       <input type="text" 
+      placeholder='Enter your task'
       onChange={handelChange}
-      placeholder='write your task' 
-      value={newTasks}
+       value={newTasks}
       />
-      <button onClick={handelAdd} >Add </button>
-      <ul style={{margin:"5%"}} >
-      {tasks.map((task)=>(
-        <li style={{ textDecoration: task.completed ? "line-through" : "none" }}
- key={task.id} > {task.text} 
-         <button onClick={()=>handelDone(task.id)} >✅</button> 
-         <button onClick={()=>handelDelete(task.id)} >❌</button></li>
-      ))}
-
-        
-
-
+      <button onClick={handelAdd}>Add</button>
+      <ul>
+        {tasks.map((task)=> 
+        <li key={task.id}> 
+         <button onClick={()=> handelDone(task.id)} style={{ textDecoration: task.completed ? "line-through" : "none" }}> ✅ {task.text}</button> 
+          <button onClick={ ()=> handelDelete (task.id)} >❌</button> </li> )}
       </ul>
     </div>
   );
