43ba99b0200685b82d9a37d5c39c216edaf44850
sagar poudel <poudelsagar@icloud.com>
2025-08-20 20:56:04 +0900

    done like app also

---
+++ Like-App/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState } from 'react';
+
+
+const  App = () => {
+  const [like , setLike] = useState(false);
+  const [count, setCount] = useState(0);
+   const handelLike = () =>{
+    if(like){
+    setLike(false);
+    setCount((prev) => prev -1 )
+    }else{
+      setLike (true);
+      setCount((prev)=> prev + 1 )
+    }
+   }
+     return (
+    <div style={{
+      textAlign:"center", 
+       height: "100vh",
+        width: "95vw",
+        padding: "20px",
+        textAlign: "center",
+      
+      }} >
+     <h1> Like App</h1>
+     <button onClick={handelLike} > {like ? "❤️" : "🤍"} </button>
+     <h1> {count} </h1>
+    </div>
+  );
+};
+
+export default  App
