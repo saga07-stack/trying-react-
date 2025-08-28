65f49bd3e8660b7a6bd815f8beb3b3ea92a89d2d
sagar poudel <poudelsagar@icloud.com>
2025-08-28 23:20:10 +0900

    undone tailwindcss

--- 17-form-app/src/App.jsx
+++ 17-form-app/src/App.jsx
@@ -1,48 +1,98 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useState } from 'react';
 
 const  App = () => {
-  const [formDate, setFormData] = useState({ id: Date.now(), name: "", email: ""});
-  const [formDatas, setFormDatas] = useState("");
-  const handelChange = (e)=>{
-   const {name, value} = e.target;
-   setFormData ({...formDatas, [name]:value , id: Date.now()})
-   
-  }
-const handelSubmit= (e)=>{
+  const [formData, setFormData] = useState ({ 
+    id: "",
+     name: "", 
+     email: "",
+     doctor: "",
+     SEX: "",
+     problem:"",
+     date:"",
+     
+     
+     });
+  const [formDatas, setFormDatas ] = useState([]);
+ 
+const handelChange = (e)=>{
+const {name, value} = e.target;
+setFormData({...formData, id: Date.now()+ Math.random() , [name]:value})
+};
+const handelSubmit =(e)=>{
   e.preventDefault();
-  setFormDatas([...formDatas , formDate])
-  setFormData({id:"", name:"", email:""})
+  setFormDatas([...formDatas, formData]);
+  setFormData({id:"",name:"", Age:"",phone:"",SEX:"", doctor:"", problem:"", dates:"",})
+  
 }
-const handelDelete = (id) =>{
-  setFormDatas(formDatas.filter((data)=> data.id === id ));
+const handelDelete = (id)=>{
+ setFormDatas (  formDatas.filter((data)=>
+    data.id !== id 
+  ))
 }
-console.log(formDate)
+console.log(formDatas)
   return (
     <div>
-      <h1>form</h1>
-     
-      <form action="" 
-      onSubmit={handelSubmit}>
-         <label htmlFor="">name :</label>
-        <input  onChange={handelChange} type="text " 
+      <h1>form page</h1>
+      <form action="" onSubmit={handelSubmit} >
+        <label htmlFor="">name: </label>
+        <input type="text"
+        placeholder='name' 
         name='name'
-        placeholder='name'
-         /> 
-         <br />
-          <label htmlFor="">email :</label>
-        <input  onChange={handelChange} type="text " 
-        name='email'
-        placeholder='email'
-         /> 
-         <button type='submit' >Submit</button>
+        onChange={handelChange}
+       value={formData.name} />
+        <br />
+        <label htmlFor="">Age: </label>
+        <input value={formData.Age} type="text"
+        placeholder='Age' 
+        name='Age'
+        onChange={handelChange}/>
+
+        <br />
+        <label htmlFor="">phone: </label>
+        <input value={formData.phone} type="text"
+        placeholder='phone' 
+        name='phone'
+        onChange={handelChange}/>
+
+        <br />
+        <label htmlFor="">SEX: </label>
+        <input value={formData.SEX} type="text"
+        placeholder='SEX' 
+        name='SEX'
+        onChange={handelChange}/>
+
+        <br />
+        <label htmlFor="">Doctor: </label>
+        <input value={formData.doctor} type="text"
+        placeholder='doctor' 
+        name='doctor'
+        onChange={handelChange}/>
+
+        <br />
+        <label htmlFor="">problem: </label>
+        <input value={formData.problem} type="text"
+        placeholder='problem' 
+        name='problem'
+        onChange={handelChange}/>
+
+        <br />
+        <label htmlFor="">date: </label>
+        <input value={formData.date} type="text"
+        placeholder='date' 
+        name="date"
+        onChange={handelChange}/>
+        <button  >Add</button>
       </form>
       <ul>
         {formDatas.map((data)=>(
-          <li key={data.id} > {date.name}  <button onClick={()=>handelDelete(data.id)} > Delete</button> </li>
+          <li key={data.id} > Name:  {data.name } AGE :  {data.Age}  phone - {data.phone}
+            Sex: {data.SEX} Doctor: {data.Doctor}  Problem:  {data.problem}  Date:  {data.Date} 
+          <button onClick={()=>handelDelete(data.id)} >Delete</button> </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default  App
+export default  App;
